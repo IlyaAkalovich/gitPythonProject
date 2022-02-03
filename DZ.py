1ab0fb9b685d7f9c90abca5ec6a75001c1cdd10d
name <namebk.ru>
2022-02-03 19:53:27 +0300

    Homework with function for counting vowels and consonants

---
+++ DZ.py
@@ -0,0 +1,20 @@
+def count(st):
+    gl = ["a", "e", "i", "u", "o", "y"]
+    glcount = 0
+    soglcount = 0
+    for i in st:
+        if i.isalpha():
+            if i in gl:
+                glcount += 1
+            else:
+                soglcount+=1
+    return glcount,soglcount
+
+def print_output(res):
+    print("Кол-во гласных: ", res[0])
+    print("Кол-во согласных: ", res[1])
+
+st = input("Введите строку: ")
+res = count(st)
+print_output(res)
+
